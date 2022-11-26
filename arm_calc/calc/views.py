d93d0b65e4200eb5cb5b7552875f7889b5e2bde5
Андрей Голишевский <a@vg9.ru>
2022-11-26 15:51:17 +0300

    add views element and result

--- arm_calc/calc/views.py
+++ arm_calc/calc/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from calc.models import Element, Rod
+
+User = get_user_model()
+
+
+def element(request, element_id):
+    element = get_object_or_404(Element, pk=element_id)
+    rods = Rod.objects.filter(element=element)
+    context = {
+        'element': element,
+        'rods': rods,
+    }
+    return render(request, 'calc/element.html', context)
+
+
+def result(request, element_id):
+    context = {
+        element_id: element_id,
+    }
+    return render(request, 'calc/result.html', context)
