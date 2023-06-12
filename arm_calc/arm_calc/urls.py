c2a6ad715d91cba1d1e32857de6cc92f8695bc8d
Андрей Голишевский <agolishevskiy@yandex.ru>
2023-06-12 22:35:52 +0300

    add error pages

--- arm_calc/arm_calc/urls.py
+++ arm_calc/arm_calc/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -7,3 +9,12 @@ urlpatterns = [
     path('auth/', include('django.contrib.auth.urls')),
     path('', include('calc.urls', namespace='calc')),
 ]
+
+handler404 = 'core.views.page_not_found'
+handler500 = 'core.views.server_error'
+handler403 = 'core.views.permission_denied'
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.STATIC_URL, document_root=settings.STATIC_ROOT
+    )
