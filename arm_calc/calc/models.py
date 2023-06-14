4412dce0ea3c57432a0465bf0b3b052e659f1f82
Andrew Golishevskiy <gav@gaver.ru>
2023-06-14 12:58:26 +0300

    pep8

--- arm_calc/calc/models.py
+++ arm_calc/calc/models.py
@@ -230,8 +230,8 @@ class Rod(PartModel):
         self.length = round(length, 3)
 
     def calculate_mass_of_single_rod(self):
-        self.mass_of_single_rod = (self.MASS_OF_METER.get(self.diameter) 
-                                  * self.length / self.MM_IN_M)
+        self.mass_of_single_rod = (self.MASS_OF_METER.get(self.diameter)
+                                   * self.length / self.MM_IN_M)
 
     def calculate_mass_of_rods(self):
         self.mass_of_rods = round(self.mass_of_single_rod * self.quantity, 3)
