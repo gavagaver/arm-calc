ff80bb6a4c1a8626dbbd4a80cb4f597ed588a183
Андрей Голишевский <agolishevskiy@yandex.ru>
2023-06-17 09:02:12 +0300

    add docs

--- arm_calc/core/views.py
+++ arm_calc/core/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 
 
 def page_not_found(request, exception):
+    """
+    Renders a 404 error page with the path of the requested URL.
+    """
     return render(
         request,
         template_name='core/404.html',
@@ -11,6 +14,9 @@ def page_not_found(request, exception):
 
 
 def csrf_failure(request, reason=''):
+    """
+    Renders a 403 error page for CSRF failures.
+    """
     return render(
         request,
         template_name='core/403csrf.html',
@@ -18,6 +24,9 @@ def csrf_failure(request, reason=''):
 
 
 def server_error(request):
+    """
+    Renders a 500 error page for server errors.
+    """
     return render(
         request,
         template_name='core/500.html',
@@ -26,6 +35,9 @@ def server_error(request):
 
 
 def permission_denied(request, exception):
+    """
+    Renders a 403 error page for permission denied errors.
+    """
     return render(
         request,
         template_name='core/403.html',
