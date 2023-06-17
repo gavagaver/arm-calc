395e3037a71c875505e4ed4d5f2f21be5426e89c
Андрей Голишевский <agolishevskiy@yandex.ru>
2023-06-17 08:55:50 +0300

    add docs

--- arm_calc/core/templatetags/user_filters.py
+++ arm_calc/core/templatetags/user_filters.py
@@ -5,4 +5,8 @@ register = template.Library()
 
 @register.filter
 def addclass(field, css):
+    """
+    Adds the specified CSS classes to the attributes
+    of the given form field widget.
+    """
     return field.as_widget(attrs={'class': css})
