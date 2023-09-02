749c28215009556cf164f8ea0042e685cb9a8753
Андрей Голишевский <agolishevskiy@yandex.ru>
2023-09-02 17:14:33 +0300

    Added custom template filter "multiply".

--- arm_calc/core/templatetags/user_filters.py
+++ arm_calc/core/templatetags/user_filters.py
@@ -10,3 +10,11 @@ def addclass(field, css):
     of the given form field widget.
     """
     return field.as_widget(attrs={'class': css})
+
+
+@register.filter
+def multiply(value, arg):
+    """
+    Multiplies the given value by the given argument.
+    """
+    return value * arg
